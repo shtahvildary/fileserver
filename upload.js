14be86cd840339dd5c293a1858af3326862416d5
shtahvildary <shtahvildary@gmail.com>
2018-03-16 00:13:26 +0330

    upload files in http

--- upload.js
+++ upload.js
@@ -1,5 +1,6 @@
 const express = require("express");
 //To download from telegram
+var http = require("http");
 var https = require("https");
 //To write to file
 var fs = require("fs");
@@ -77,6 +78,59 @@ app.post("/upload", function(req, res) {
 
   //DONE :)
 });
+//robot request this when it wants to upload sth.
+app.post("/upload/http", function(req, res) {
+  //check clientid
+  if (req.headers["clientid"] != "123")
+    res.status(401).json({
+      error: "clientid is wrong."
+    });
+  //it's like >> var link=req.body.link;
+  var { link } = req.body;
 
+  //https://api.telegram.org/file/bot449968526:AAGY4Tz48MiN8uxUD_0nWHFZSQscD9OQ_Vk/videos/file_19.mp4
+
+  //export path to store from telegram link
+  var filePath = url.parse(link).pathname;
+  //this step filePath is >> /file/bot449968526:AAGY4Tz48MiN8uxUD_0nWHFZSQscD9OQ_Vk/<our_path> and we want <our_path>
+
+  //admin it-word cloud
+  // var bot = botgram("456299862:AAGB1q_AMolsLpeE5EARolW4FHEi5-1kqjE");
+  // filePath = filePath.replace("/file/bot456299862:AAGB1q_AMolsLpeE5EARolW4FHEi5-1kqjE/", "");
+
+  //shmt_bot
+  // //var botToken = "449968526:AAGY4Tz48MiN8uxUD_0nWHFZSQscD9OQ_Vk";
+  // filePath = filePath.replace("/file/bot449968526:AAGY4Tz48MiN8uxUD_0nWHFZSQscD9OQ_Vk/", "");
+
+  //iribnews
+  //// var botToken = "545443179:AAGEKFAT_mg5H2aTZbCKEPXr2Pkee11b8l4";
+  filePath = filePath.replace(
+    "/file/bot545443179:AAGEKFAT_mg5H2aTZbCKEPXr2Pkee11b8l4/",
+    ""
+  );
+
+  // //Solving index problem for ogg files
+  // if (!filePath.includes('.')) {
+  //     filePath += ".ogg";
+  // }
+
+  var subfolder = filePath.substring(0, filePath.lastIndexOf("/"));
+  var dir = "./files/" + subfolder;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  //create a stream to write file on our storage
+  var file = fs.createWriteStream("./files/" + filePath);
+  console.log(link)
+  //get file
+  var request = http.get(link, function(response) {
+    //store it with created write stream
+    response.pipe(file);
+
+    return res.status(200).json({filePath: filePath})
+  });
+
+  //DONE :)
+});
 //server will listen on 9001 port for requests.
 app.listen(9001, () => console.log("Upload server listening on port 9001!"));
